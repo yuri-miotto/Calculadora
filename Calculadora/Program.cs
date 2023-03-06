374a98bb7d1e98fd18e6cf31897f8499f4485730
yuri-miotto <yurimagrinimiotto@gmail.com>
2023-03-06 09:56:35 -0300

    agora as funções tem um retorno

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,10 +4,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        double num1, num2;
+        double n1, n2;
         int opcao = 0;
-
-        void Menu()
+        int Menu()
         {
             Console.Clear();
             Console.WriteLine("1 - Somar");
@@ -15,83 +14,66 @@ internal class Program
             Console.WriteLine("3 - Multiplicar");
             Console.WriteLine("4 - Dividir");
             Console.WriteLine("5 - Sair");
-            Console.WriteLine("Escolha uma opção: ");
-            opcao = int.Parse(Console.ReadLine());
-
-            switch (opcao)
-            {             
-
-                case 1:
-                    Somar();
-                    Console.ReadLine();
-                    break;
-
-                case 2:
-                    Subtrair();
-                    Console.ReadLine();
-                    break;
-
-                case 3:
-                    Multiplicar();
-                    Console.ReadLine();
-                    break;
-
-                case 4:
-                    Dividir();
-                    Console.ReadLine();
-                    break;
-
-                case 5:
-                    Console.WriteLine("Até logo!");
-                    Console.ReadLine();
-                    break;
-
+            Console.Write("Escolha uma opção: ");
+            return opcao = int.Parse(Console.ReadLine());
+        }
+        double Soma()
+        {
+            return n1 + n2;
+        }
+        double Subtracao()
+        {
+            return n1 - n2;
+        }
+        double Divisao()
+        {
+            return n1 / n2;
+        }
+        double Multiplicacao()
+        {
+            return n1 * n2;
+        }
+        Console.WriteLine("Informe o primeiro número: ");
+        n1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o segundo número: ");
+        n2 = double.Parse(Console.ReadLine());
+        while (opcao != 5)
+        {
+            switch (Menu())
+            {
                 default:
                     Console.WriteLine("Opção inválida!");
                     Console.ReadLine();
                     break;
-            }
-        }
-
-        Console.WriteLine("Informe o primeiro número: ");
-        num1 = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Informe o segundo número: ");
-        num2 = double.Parse(Console.ReadLine());
-
-        while (opcao != 5)
-        {
-            Menu();
-        }
-
-        void Somar()
-        {
-            double resultado = num1 + num2;
-            Console.WriteLine("A soma dos números é igual a: " + resultado);
-        }
-
-        void Subtrair()
-        {
-            double resultado = num1 - num2;
-            Console.WriteLine("A subtração dos números é igual a: " + resultado);
-        }
-
-        void Multiplicar()
-        {
-            double resultado = num1 * num2;
-            Console.WriteLine("A multiplicação dos números é igual a: " + resultado);
-        }
-
-        void Dividir()
-        {
-            if (num2 == 0)
-            {
-                Console.WriteLine("Não é possível dividir por zero!");
-            }
-            else
-            {
-                double resultado = num1 / num2;
-                Console.WriteLine("A divisão dos números é igual a: " + resultado);
+                case 1:
+                    Console.WriteLine("A soma dos números é: {0}", Soma());
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("A subtração dos números é: {0}", Subtracao());
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    Console.WriteLine("A multiplicação dos números é: {0}", Multiplicacao());
+                    Console.ReadLine();
+                    break;
+                case 4:
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine("Não é possível dividir por zero!");
+                        Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("A divisão dos números é: {0}", Divisao());
+                        Console.ReadLine();
+                        break;
+                    }
+                case 5:
+                    Console.WriteLine("Fim do programa!");
+                    Console.ReadLine();
+                    break;
             }
         }
     }
